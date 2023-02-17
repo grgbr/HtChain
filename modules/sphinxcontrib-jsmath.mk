421a32999ae766b56e29868c5ba7d9946ad6ba1c
Gregor Boirie <gregor.boirie@free.fr>
2023-02-17 15:35:18 +0100

    sphinxcontrib-jsmath: move to refined python module build macros

--- modules/sphinxcontrib-jsmath.mk
+++ modules/sphinxcontrib-jsmath.mk
@@ -49,30 +49,27 @@ endef
 # Staging definitions
 ################################################################################
 
-$(call gen_deps,stage-sphinxcontrib-jsmath,stage-wheel)
-$(call gen_check_deps,stage-sphinxcontrib-jsmath,stage-pytest stage-sphinx)
-
 check_stage-sphinxcontrib-jsmath = \
 	$(call sphinxcontrib-jsmath_check_cmds,\
 	       stage-sphinxcontrib-jsmath)
+
+$(call gen_deps,stage-sphinxcontrib-jsmath,stage-wheel)
+$(call gen_check_deps,stage-sphinxcontrib-jsmath,stage-pytest stage-sphinx)
 $(call gen_python_module_rules,stage-sphinxcontrib-jsmath,\
                                sphinxcontrib-jsmath,\
-                               $(stagedir),\
-                               ,\
-                               check_stage-sphinxcontrib-jsmath)
+                               $(stagedir))
 
 ################################################################################
 # Final definitions
 ################################################################################
 
-$(call gen_deps,final-sphinxcontrib-jsmath,stage-wheel)
-$(call gen_check_deps,final-sphinxcontrib-jsmath,stage-pytest stage-sphinx)
-
 check_final-sphinxcontrib-jsmath = \
 	$(call sphinxcontrib-jsmath_check_cmds,\
 	       final-sphinxcontrib-jsmath)
+
+$(call gen_deps,final-sphinxcontrib-jsmath,stage-wheel)
+$(call gen_check_deps,final-sphinxcontrib-jsmath,stage-pytest stage-sphinx)
 $(call gen_python_module_rules,final-sphinxcontrib-jsmath,\
                                sphinxcontrib-jsmath,\
                                $(PREFIX),\
-                               $(finaldir),\
-                               check_final-sphinxcontrib-jsmath)
+                               $(finaldir))
